a0c3ef40c0caa4fb2f8e9deb66ace57c2bbf28b8
Quang <hongquang7302@gmail.com>
2023-06-01 19:32:18 +0700

    push

--- landing-page/src/data/testimonialsData.js
+++ landing-page/src/data/testimonialsData.js
@@ -1,25 +1,35 @@
-import image1 from "../assets/t-image1.png";
-import image2 from "../assets/t-image2.jpg";
-import image3 from "../assets/t-image3.jpg";
+import image1 from "../assets/Hieu.jpeg";
+import image2 from "../assets/Khoa.jpeg";
+import image3 from "../assets/Hiep.jpeg";
+import image4 from "../assets/Quang.jpeg";
 
 export const testimonialsData = [
   {
     image: image1,
     review:
-      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+      "I made the right choice by choosing the Limitless and by choosing the right plan and program I already achieved my ideal body!",
+    name: "Hieu Nguyen",
+    status: "ENTREPRENEUR",
+  },
+  {
+    image: image4,
+    review:
+      "Limitless is a variety of exercises and easy to do, divided into three levels for beginner, advanced and advanced. The app also calculates calories burned and exercise time in the form of a visual graph.",
+    name: "Quang Pham",
+    status: "CEO",
   },
   {
     image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
-    status: 'COACH'
+    review:
+      "This is an ideal place for us to train ourselves. Come and have an interesting experience for yourself.",
+    name: "Khoa Dang",
+    status: "COACH",
   },
   {
-    image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
-    status: "CUSTOMER"
-  }
+    image: image3,
+    review:
+      "I am very satisfied with this app and it really gives me a great experience when training.",
+    name: "Hiep Dang",
+    status: "CUSTOMER",
+  },
 ];
